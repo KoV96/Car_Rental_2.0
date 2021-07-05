efa5e78b8ac176bf0528cced8702dad9d7696b8a
Oleksandr Kurylyk <oleksandr.kurylyk@dxc.com>
2021-07-05 17:08:10 +0300

    Have successfully created registration form. But still have one error while starting spring boot application, connected with relations between entities in hibernate: "Referencing column 'user_id' and referenced column 'user_id' in foreign key constraint 'FK7t0uo7yxjck29e967rny84ky4' are incompatible."

---
+++ src/main/java/com/spring/learning/car_rental_20/controller/MainController.java
@@ -0,0 +1,33 @@
+package com.spring.learning.car_rental_20.controller;
+
+import com.spring.learning.car_rental_20.model.User;
+import com.spring.learning.car_rental_20.repos.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class MainController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("")
+    public String homePage(){
+        return "home";
+    }
+
+    @GetMapping("/register")
+    public String registrationPage(Model model){
+        model.addAttribute("user", new User());
+        return "register_form";
+    }
+
+    @PostMapping("/process_register")
+    public String processRegistration(User user){
+        userRepository.save(user);
+        return "register_success";
+    }
+}
